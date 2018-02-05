6632a244128940d8ec162b80b728c6321b05976b
coreytegeler <coreytegeler@gmail.com>
2018-02-04 22:36:20 -0500

    includes, new specs

--- script.js
+++ script.js
@@ -79,20 +79,20 @@ $ = jQuery;
 
 $(function () {
   $('body').on('click', '.accordion a', function (e) {
-    var $accordion, $inside, $li, $link, $opened, insideHeight, linkHeight, newHeight;
+    var $accordion, $inside, $link, $opened, $wrapper, insideHeight, linkHeight, newHeight;
     e.preventDefault();
     $link = $(this);
-    $li = $link.parents('li');
-    $inside = $li.find('.inside');
-    $accordion = $li.parents('.accordion');
+    $wrapper = $link.parents('.content_wrapper');
+    $inside = $wrapper.find('.inside');
+    $accordion = $wrapper.parents('.accordion');
     linkHeight = $link.innerHeight();
-    insideHeight = $inside.find('.content').innerHeight();
-    console.log($inside);
-    if ($li.is('.opened')) {
+    insideHeight = $inside.find('.hidden_content').innerHeight();
+    if ($wrapper.is('.opened')) {
       newHeight = 0;
     } else {
       newHeight = insideHeight;
       if ($opened = $accordion.find('.opened')) {
+        console.log($opened);
         $opened.removeClass('opened');
         $opened.find('.inside').css({
           height: 0
@@ -102,7 +102,8 @@ $(function () {
     $inside.css({
       height: newHeight
     });
-    return $li.toggleClass('opened');
+    console.log($wrapper);
+    return $wrapper.toggleClass('opened');
   });
   return $('.inline_nav.toc a').on('click', function (e) {
     var $header, $nav, hash, target, top;
